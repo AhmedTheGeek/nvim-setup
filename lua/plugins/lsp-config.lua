7206ccea9322f7a827d8b463aa42e1230ff6a2d4
Ahmed Hussein <me@ahmedgeek.com>
2025-02-04 13:16:26 +0200

    Adding peak.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,48 +1,64 @@
-return  { {"williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end },
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-          ensure_installed = { "lua_ls", "intelephense", "ts_ls", "cssls", "ts_ls", "vuels", "html", "yamlls", "dockerls", "cmake", "terraformls", "jsonls"}
+        ensure_installed = {
+          "lua_ls",
+          "intelephense",
+          "ts_ls",
+          "cssls",
+          "ts_ls",
+          "vuels",
+          "html",
+          "yamlls",
+          "dockerls",
+          "cmake",
+          "terraformls",
+          "jsonls",
+        },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      lspconfig.lua_ls.setup({capabilities = capabilities})
-      lspconfig.intelephense.setup({capabilities = capabilities})
-      lspconfig.ts_ls.setup({capabilities = capabilities})
-      lspconfig.cssls.setup({capabilities = capabilities})
-      lspconfig.vuels.setup({capabilities = capabilities})
-      lspconfig.html.setup({capabilities = capabilities})
-      lspconfig.yamlls.setup({capabilities = capabilities})
-      lspconfig.dockerls.setup({capabilities = capabilities})
-      lspconfig.cmake.setup({capabilities = capabilities})
-      lspconfig.terraformls.setup({capabilities = capabilities})
-      lspconfig.jsonls.setup({capabilities = capabilities})
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-    end
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.intelephense.setup({ capabilities = capabilities })
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
+      lspconfig.cssls.setup({ capabilities = capabilities })
+      lspconfig.vuels.setup({ capabilities = capabilities })
+      lspconfig.html.setup({ capabilities = capabilities })
+      lspconfig.yamlls.setup({ capabilities = capabilities })
+      lspconfig.dockerls.setup({ capabilities = capabilities })
+      lspconfig.cmake.setup({ capabilities = capabilities })
+      lspconfig.terraformls.setup({ capabilities = capabilities })
+      lspconfig.jsonls.setup({ capabilities = capabilities })
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+    end,
   },
   {
     "bitpoke/wordpress.nvim",
     config = function()
-      local wp = require('wordpress')
-      local lspconfig = require('lspconfig')
+      local wp = require("wordpress")
+      local lspconfig = require("lspconfig")
 
       -- setup intelephense for PHP, WordPress and WooCommerce development
       lspconfig.intelephense.setup(wp.intelephense)
-
-    end
+    end,
   },
 }
