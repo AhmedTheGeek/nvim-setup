96d23e0279acf7308d85d6aa99c14f20be40fdf6
Ahmed Hussein <me@ahmedgeek.com>
2025-03-23 08:43:40 +0200

    Plugins: add tokyonight.

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set clipboard=unnamedplus")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+-- vim.cmd([[colorscheme tokyonight]])
 vim.g.mapleader = " "
 vim.opt.autoread= true
 -- Move to previous/next buffer
