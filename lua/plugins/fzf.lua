c41c0702c62ad58de403344989cbc018feb7e34d
Ahmed Hussein <me@ahmedgeek.com>
2025-04-09 23:57:28 +0200

    Plugins: add fzf.

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,13 @@
+return {
+  "ibhagwan/fzf-lua",
+  -- optional for icon support
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- or if using mini.icons/mini.nvim
+  -- dependencies = { "echasnovski/mini.icons" },
+  opts = {},
+  config = function()
+    require("fzf-lua").setup({
+      vim.keymap.set("n", "<leader>fg", require("fzf-lua").live_grep),
+    })
+  end,
+}
