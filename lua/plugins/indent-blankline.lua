0b38d6a9f048c017d2f2c5a5dafcef24f40188b7
Ahmed Hussein <me@ahmedgeek.com>
2025-09-23 22:53:17 +0300

    Indent: Change indent color.

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,39 +3,46 @@ return {
 	main = "ibl",
 	event = { "BufReadPost", "BufNewFile" },
 	config = function()
-		local highlight = {
-			"RainbowRed",
-			"RainbowYellow",
-			"RainbowBlue",
-			"RainbowOrange",
-			"RainbowGreen",
-			"RainbowViolet",
-			"RainbowCyan",
-		}
-
 		local hooks = require("ibl.hooks")
+
+		-- Setup dim indent lines and highlighted scope
 		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+			-- Dim indent lines (use a subtle gray)
+			vim.api.nvim_set_hl(0, "IblIndent", { fg = "#3B4048" })
+
+			-- Highlighted scope colors for current indent level
+			vim.api.nvim_set_hl(0, "IblScopeRed", { fg = "#E06C75" })
+			vim.api.nvim_set_hl(0, "IblScopeYellow", { fg = "#E5C07B" })
+			vim.api.nvim_set_hl(0, "IblScopeBlue", { fg = "#61AFEF" })
+			vim.api.nvim_set_hl(0, "IblScopeOrange", { fg = "#D19A66" })
+			vim.api.nvim_set_hl(0, "IblScopeGreen", { fg = "#98C379" })
+			vim.api.nvim_set_hl(0, "IblScopeViolet", { fg = "#C678DD" })
+			vim.api.nvim_set_hl(0, "IblScopeCyan", { fg = "#56B6C2" })
 		end)
 
 		require("ibl").setup({
 			indent = {
-				highlight = highlight,
+				-- Use dim highlight for all indent lines
+				highlight = "IblIndent",
 				char = "│",
 				tab_char = "│",
 			},
 			scope = {
+				-- Enable scope highlighting for current indent level
 				enabled = true,
 				show_start = true,
 				show_end = false,
 				injected_languages = true,
-				highlight = highlight,
+				-- Use colored highlights for the current scope
+				highlight = {
+					"IblScopeRed",
+					"IblScopeYellow",
+					"IblScopeBlue",
+					"IblScopeOrange",
+					"IblScopeGreen",
+					"IblScopeViolet",
+					"IblScopeCyan",
+				},
 				priority = 500,
 			},
 			exclude = {
